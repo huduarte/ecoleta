54a05426c72bf914d6ed289079a798bb16b8fa68
Hudson Duarte <hudsoneeto@outlook.com>
2020-06-28 15:31:53 -0300

    template engine

--- src/server.js
+++ src/server.js
@@ -5,26 +5,31 @@ const server = express()
 //configurar pasta pública
 server.use(express.static("public"))
 
+
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server, 
+    noCache: true
+})
+
 //configurar caminhos da minha aplicação
 
 //pagina inicial
 //req: Requisição
 //res: Resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html", { title: "Um título"})
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 
 server.get("/search-results", (req, res) => {
-    res.sendFile(__dirname + "/views/search-results.html")
+    return res.render("search-results.html")
 })
-
-//PAREI NO MINUTO 57:50
-
-
 
 //ligar o servidor
 
