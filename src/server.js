e79f17a16a42b550e2818d669df9b5abde7b8ca2
Hudson Duarte <hudsoneeto@outlook.com>
2020-06-30 12:26:34 -0300

    //

--- src/server.js
+++ src/server.js
@@ -19,37 +19,37 @@ nunjucks.configure("src/views", {
 //pagina inicial
 //req: Requisição
 //res: Resposta
-// server.get("/", (req, res) => {
-//     return res.render("index.html", { title: "Um título"})
-// })
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "Um título"})
+})
 
-// server.get("/create-point", (req, res) => {
+server.get("/create-point", (req, res) => {
     
-//     //req.query: Query Strings da nossa url
-//     console.log(req.query)
+    //req.query: Query Strings da nossa url
+    console.log(req.query)
 
-//     return res.render("create-point.html")
-// })
+    return res.render("create-point.html")
+})
 
-// server.get("/search-results", (req, res) => {
+server.get("/search-results", (req, res) => {
 
-//     //pegar os dados do banco de dados
-//     db.all(`SELECT * FROM places`, function(err, rows){
-//        if (err){
-//             return console.log(err)
-//         }
-//         const total = rows.length
+    //pegar os dados do banco de dados
+    db.all(`SELECT * FROM places`, function(err, rows){
+       if (err){
+            return console.log(err)
+        }
+        const total = rows.length
 
-//         //console.log("Aqui estão seus registros")
-//         //console.log(rows)
+        //console.log("Aqui estão seus registros")
+        //console.log(rows)
 
-//         // mostrar a pagia html com os dados do banco de dados
-//         return res.render("search-results.html", { places: rows, total: total})
-//     })
+        // mostrar a pagia html com os dados do banco de dados
+        return res.render("search-results.html", { places: rows, total: total})
+    })
 
 
-// })
+})
 
-// //ligar o servidor
+//ligar o servidor
 
-// server.listen(3000)
+server.listen(3000)
